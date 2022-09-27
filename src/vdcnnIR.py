fix softmax

src/vdcnnIR.py
@@ -122,7 +122,7 @@ class Vgg(nn.Module):
         fc_layers.extend([nn.Linear(in_features=2*2*(8*base_features), out_features= base_features*base_features),nn.ReLU()])
         fc_layers.extend([nn.Linear(in_features=base_features*base_features, out_features= base_features*base_features),nn.ReLU()])
         fc_layers.append(self.save_layer)
-        fc_layers.extend([nn.Linear(in_features=base_features*base_features, out_features= self.num_classes)])
+        fc_layers.extend([nn.Linear(in_features=base_features*base_features, out_features= self.num_classes), nn.Softmax(dim = 1)])
         self.layers = nn.Sequential(*layers)
         self.fc_layers = nn.Sequential(*fc_layers)
         if initialize_weights:
@@ -147,14 +147,9 @@ class Vgg(nn.Module):
         output = output.view(output.size(0), -1)
         # print(output.size())
         output = self.fc_layers(output)
+        #print(output)
         maps = self.save_layer.maps
         self.save_layer.maps = []
-        print("maps 0: ", maps[0].size())
-        print("maps 1: ", maps[1].size())
-        print("maps 2: ", maps[2].size())
-        print("maps 3: ", maps[3].size())
-        print("maps 4: ", maps[4].size())
-        print("maps 5: ", maps[5].size())
         return output, maps
 
 

train.py
@@ -21,7 +21,7 @@ def get_args():
     parser = argparse.ArgumentParser("""Very Deep Convolutional Networks for Large Scale Image Recognition""")
     parser.add_argument('-t', '--train', type=str, default='root', help="""required image dataset for training a model.
                                                                            It must be in the data directory """)
-    parser.add_argument('-v', '--val', type=str, default='root', help="""required image dataset for training a model.
+    parser.add_argument('-v', '--val', type=str, default='root_val', help="""required image dataset for training a model.
                                                                               It must be in the data directory """)
     parser.add_argument('-b', '--batchsize', type=int, choices=[64,128,256, 512], default=64, help='select number of samples to load from dataset')
     parser.add_argument('-e', '--epochs', type=int, choices=[50, 100, 150], default=50)
@@ -120,8 +120,8 @@ def train(opt):
             prob, maps_t = model(data_)
             # print(prob)
             prob_ = np.argmax(prob.detach().cpu(), -1)
-            print("prob: ", prob.size())
-            print("label: ", label.size())
+            
+            #print("label: ", label)
 
             loss = criterion(prob, label)
             train_loss.append(loss.item()*len(label.cpu()))